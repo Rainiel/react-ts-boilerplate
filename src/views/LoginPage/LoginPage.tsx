c4a1a5cfac52efb25cef75539f6300782a438f91
Rainiel <villanuevarainiel@gmail.com>
2021-01-13 15:22:33 +0800

    login form in middle of page

--- src/views/LoginPage/LoginPage.tsx
+++ src/views/LoginPage/LoginPage.tsx
@@ -1,22 +1,73 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
+import Input from "@material-ui/core/Input";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { login } from "../../services/auth";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+    loginForm: {
+      borderRadius: "8px",
+      backgroundColor: "#ffffff",
+    },
+    background: {
+      backgroundColor: "#f2f2f2",
+    },
+  })
+);
 
 interface prop {}
 
 const LoginPage: React.FC<any> = (props) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <button
-        onClick={() => {
-          login(() => {
-            props.history.push('/layout1');
-          });
-        }}
+    <Box
+      display="flex"
+      width={1}
+      height={1}
+      justifyContent="center"
+      alignItems="center"
+      className={classes.background}
+    >
+      <Box
+        display="flex"
+        bgcolor="background.paper"
+        className={classes.loginForm}
       >
-        Login
-      </button>
-    </div>
+        <Box p={8}>
+          <form className={classes.root} noValidate autoComplete="off">
+            <Box>
+              <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </Box>
+            <Box>
+              <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </Box>
+            <Box textAlign='center'>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  // document.body.style.backgroundColor = "black";
+                  login(() => {
+                    props.history.push("/layout1");
+                  });
+                }}
+              >
+                Login
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
